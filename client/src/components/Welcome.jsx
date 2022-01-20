31bce2ee87ad9f044672f24c025a2e4fc0635971
Timgo-1 <timgodesigns@gmail.com>
2022-01-20 07:14:28 +0300

    update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -41,7 +41,7 @@ const Welcome = () => {
             Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-pink-200 font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell cryptocurrencies easily on Timcryptex.
+            Explore the crypto world. Buy and sell cryptocurrencies easily on TimcryptEx.
           </p>
           {!currentAccount && (
             <button
